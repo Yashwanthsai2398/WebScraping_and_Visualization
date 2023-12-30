45296ec6d6f493f073def7d1fde82c9830d77e02
Yashwanth sai Bathini <145509007+Yashwanthsai2398@users.noreply.github.com>
2023-12-30 18:15:26 -0500

    Update Bathini_Prog7.py

--- Bathini_Prog7.py
+++ Bathini_Prog7.py
@@ -1,12 +1,4 @@
 #YASHWANTH_SAI_BATHINI
-#1017917780
-#DATE_OF_CREATION : 05/DECEMBER/2023
-'''This Python program uses web scraping to extract mobile phone listings from eBay, creating a
-structured DataFrame. It analyzes the data to identify gadgets with the highest and lowest prices, as well as those
-with the highest and lowest ratings. It employs regular expressions to extract numerical information like ratings.
-The program visualizes the relationship between price and rating using Seaborn and Matplotlib, displaying a bar graph
-in green to represent the analyzed data for quick and clear insights into the mobile phone listings' price-rating 
-distribution.'''
 #Imported the list of multiple libraries that used in the below code
 import requests  #For making HTTP requests
 from bs4 import BeautifulSoup  # For parsing HTML content
